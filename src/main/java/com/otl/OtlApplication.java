89bbdeb1bde199bc0cd8449cf74d860db603fbc4
0Hoxy <tinyfinger@naver.com>
2024-07-17 16:20:08 +0900

    first commit

---
+++ src/main/java/com/otl/OtlApplication.java
@@ -0,0 +1,13 @@
+package com.otl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OtlApplication.class, args);
+    }
+
+}
