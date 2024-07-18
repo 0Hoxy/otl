8ec83e28945069552b3f521f001c5b62f0ee57e9
0Hoxy <tinyfinger@naver.com>
2024-07-18 09:26:09 +0900

    create Folder & User Entity

---
+++ src/main/java/com/otl/entity/User.java
@@ -0,0 +1,14 @@
+package com.otl.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+}
