1da889ccf246a327f5fd2d4a1c00cc49ea88cbac
0hoxy <tinyfinger@naver.com>
2024-07-19 16:46:41 +0900

    feature: Signup, UserData push in mysql

--- src/main/java/com/otl/controller/UserController.java
+++ src/main/java/com/otl/controller/UserController.java
@@ -1,11 +1,14 @@
 package com.otl.controller;
 
 import com.otl.dto.UserRegisterFormDTO;
+import com.otl.entity.User;
 import com.otl.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,15 +16,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/user")
 public class UserController {
     private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
 
     @GetMapping("/login")
-    public String userLogin() {
+    public String loginPage() {
         return "main";
     }
 
     @GetMapping("/register")
-    public String userRegister(Model model) {
+    public String RegisterPage(Model model) {
         model.addAttribute("UserRegisterFormDTO", new UserRegisterFormDTO());
         return "user/registerForm";
+    }
+
+    @PostMapping("/register")
+    public String userRegister(UserRegisterFormDTO userRegisterFormDTO) {
+        User user = User.createUser(userRegisterFormDTO, passwordEncoder);
+        userService.joinUser(user);
+
+        return "redirect:/";
     }
 }
