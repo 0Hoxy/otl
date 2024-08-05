b4758d833155cd0c9c424be7a56a6af80692ce83
0Hoxy <tinyfinger@naver.com>
2024-08-05 14:39:42 +0900

    Test: OrderService Test Accepted

---
+++ src/test/java/com/otl/order/OrderServiceTest.java
@@ -0,0 +1,72 @@
+package com.otl.order;
+
+import com.otl.items.constant.ItemSellStatus;
+import com.otl.items.entity.Item;
+import com.otl.items.repository.ItemRepository;
+import com.otl.order.dto.OrderDto;
+import com.otl.order.entity.Order;
+import com.otl.order.entity.OrderItem;
+import com.otl.order.repository.OrderRepository;
+import com.otl.user.entity.User;
+import com.otl.user.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@TestPropertySource(locations = "classpath:application-test.properties")
+class OrderServiceTest {
+
+    @Autowired
+    private OrderService orderService;
+    @Autowired
+    private OrderRepository orderRepository;
+    @Autowired
+    ItemRepository itemRepository;
+    @Autowired
+    UserRepository userRepository;
+
+    public Item saveItem() {
+        Item item = new Item();
+        item.setItemNm("테스트 상품");
+        item.setPrice(10000);
+        item.setItemDetail("테스트 상품 설명");
+        item.setItemSellStatus(ItemSellStatus.SELL);
+        item.setStockNumber(100);
+        return itemRepository.save(item);
+    }
+
+    public User saveUser() {
+        User user = new User();
+        user.setEmail("test@test.com");
+        return userRepository.save(user);
+    }
+
+    @Test
+    @DisplayName("주문 테스트")
+    public void order() {
+        Item item = saveItem();
+        User user = saveUser();
+
+        OrderDto orderDto = new OrderDto();
+        orderDto.setCount(10);
+        orderDto.setItemId(item.getId());
+
+        Long orderId = orderService.order(orderDto, user.getEmail());
+
+        Order order = orderRepository.findById(orderId).orElseThrow(EntityNotFoundException::new);
+
+        List<OrderItem> orderItems = order.getOrderItems();
+        int totalPrice = orderDto.getCount() * item.getPrice();
+        assertEquals(totalPrice, order.getTotalPrice());
+    }
+}
