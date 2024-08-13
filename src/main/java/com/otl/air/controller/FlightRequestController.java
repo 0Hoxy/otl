60405e26931a4948615740cc9993a31f8dd2d9c9
SUBIN <ksb010105@naver.com>
2024-08-13 16:49:01 +0900

    fixed : delete AccommodationDTO, RoomDTO

--- src/main/java/com/otl/air/controller/FlightRequestController.java
+++ src/main/java/com/otl/air/controller/FlightRequestController.java
@@ -1,64 +1,64 @@
-package com.otl.air.controller;
-
-import com.otl.air.dto.Flight;
-import com.otl.air.service.FlightRequestService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.List;
-import java.util.Locale;
-
-@Controller
-@RequestMapping("/api")
-public class FlightRequestController {
-
-    @Autowired
-    private FlightRequestService flightRequestService;
-
-    @GetMapping("/list")
-    public String searchFlights(@RequestParam(value = "departure_id", required = true, defaultValue = "PUS") String departure_id,
-                                @RequestParam(value = "arrival_id", required = true, defaultValue = "NRT") String arrival_id,
-                                @RequestParam(value = "outbound_date", required = true, defaultValue = "2024-08-20") String outbound_date,
-                                @RequestParam(value = "return_date", required = false, defaultValue = "2024-08-25") String return_date,
-                                @RequestParam(value = "travel_class", required = false, defaultValue = "0") int travel_class,
-                                @RequestParam(value = "stops", required = false, defaultValue = "0") int stops,
-                                @RequestParam(value = "max_price", required = false, defaultValue = "0") int max_price,
-                                @RequestParam(value = "hl", required = false) String hl,
-                                @RequestParam(value = "gl", required = false) String gl,
-                                @RequestParam(value = "currency", required = false) String currency,
-                                @RequestParam(value = "type", required = true, defaultValue = "1") int type, Model model
-    ) throws Exception {
-
-        Flight requestEntity = new Flight( hl, gl, type, currency, departure_id, arrival_id,
-            LocalDate.parse(outbound_date, DateTimeFormatter.ofPattern("yyyy-MM-dd", Locale.ENGLISH)),
-            return_date.equals(" ") ? null : LocalDate.parse(return_date, DateTimeFormatter.ofPattern("yyyy-MM-dd", Locale.ENGLISH)),
-            travel_class, stops, max_price);
-
-        // FlightRequestService에서 검색 요청
-        List<Flight> flights = flightRequestService.searchFlights(requestEntity);
-
-        // Add response data to the model
-        model.addAttribute("departure_id", departure_id);
-        model.addAttribute("arrival_id", arrival_id);
-        model.addAttribute("outbound_date", outbound_date);
-        model.addAttribute("return_date", return_date.equals(" ") ? null : return_date);
-        model.addAttribute("travel_class", travel_class);
-        model.addAttribute("stops", stops);
-        model.addAttribute("max_price", max_price);
-        model.addAttribute("hl", hl);
-        model.addAttribute("gl", gl);
-        model.addAttribute("currency", currency);
-        model.addAttribute("type", type);
-        model.addAttribute("flights", flights); // 모델에 flights 데이터 추가
-
-        return "pages/air/list";
-    }
-}
-
-
+//package com.otl.air.controller;
+//
+//import com.otl.air.dto.Flight;
+//import com.otl.air.service.FlightRequestService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//
+//import java.time.LocalDate;
+//import java.time.format.DateTimeFormatter;
+//import java.util.List;
+//import java.util.Locale;
+//
+//@Controller
+//@RequestMapping("/api")
+//public class FlightRequestController {
+//
+//    @Autowired
+//    private FlightRequestService flightRequestService;
+//
+//    @GetMapping("/list")
+//    public String searchFlights(@RequestParam(value = "departure_id", required = true, defaultValue = "PUS") String departure_id,
+//                                @RequestParam(value = "arrival_id", required = true, defaultValue = "NRT") String arrival_id,
+//                                @RequestParam(value = "outbound_date", required = true, defaultValue = "2024-08-20") String outbound_date,
+//                                @RequestParam(value = "return_date", required = false, defaultValue = "2024-08-25") String return_date,
+//                                @RequestParam(value = "travel_class", required = false, defaultValue = "0") int travel_class,
+//                                @RequestParam(value = "stops", required = false, defaultValue = "0") int stops,
+//                                @RequestParam(value = "max_price", required = false, defaultValue = "0") int max_price,
+//                                @RequestParam(value = "hl", required = false) String hl,
+//                                @RequestParam(value = "gl", required = false) String gl,
+//                                @RequestParam(value = "currency", required = false) String currency,
+//                                @RequestParam(value = "type", required = true, defaultValue = "1") int type, Model model
+//    ) throws Exception {
+//
+//        Flight requestEntity = new Flight( hl, gl, type, currency, departure_id, arrival_id,
+//            LocalDate.parse(outbound_date, DateTimeFormatter.ofPattern("yyyy-MM-dd", Locale.ENGLISH)),
+//            return_date.equals(" ") ? null : LocalDate.parse(return_date, DateTimeFormatter.ofPattern("yyyy-MM-dd", Locale.ENGLISH)),
+//            travel_class, stops, max_price);
+//
+//        // FlightRequestService에서 검색 요청
+//        List<Flight> flights = flightRequestService.searchFlights(requestEntity);
+//
+//        // Add response data to the model
+//        model.addAttribute("departure_id", departure_id);
+//        model.addAttribute("arrival_id", arrival_id);
+//        model.addAttribute("outbound_date", outbound_date);
+//        model.addAttribute("return_date", return_date.equals(" ") ? null : return_date);
+//        model.addAttribute("travel_class", travel_class);
+//        model.addAttribute("stops", stops);
+//        model.addAttribute("max_price", max_price);
+//        model.addAttribute("hl", hl);
+//        model.addAttribute("gl", gl);
+//        model.addAttribute("currency", currency);
+//        model.addAttribute("type", type);
+//        model.addAttribute("flights", flights); // 모델에 flights 데이터 추가
+//
+//        return "pages/air/list";
+//    }
+//}
+//
+//
--- src/main/java/com/otl/air/service/FlightRequestService.java
+++ src/main/java/com/otl/air/service/FlightRequestService.java
@@ -1,130 +1,130 @@
-package com.otl.air.service;
-
-import com.otl.air.entity.Airport;
-import com.otl.air.dto.Flight;
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestClientException;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
-
-import java.util.ArrayList;
-import java.util.List;
-
-@Service
-@PropertySource("classpath:application-dev.properties")
-public class FlightRequestService {
-
-    @Value("${GOOGLE_FLIGHTS_API_KEY}")
-    private String apiKey;
-
-    @Autowired
-    private RestTemplate restTemplate;
-
-    public List<Flight> searchFlights(Flight query) throws Exception {
-        try {
-            String baseUrl = "https://serpapi.com/search.json?engine=google_flights";
-            UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(baseUrl)
-                .queryParam("departure_id", query.getDeparture_id())
-                .queryParam("arrival_id", query.getArrival_id())
-                .queryParam("type", query.getType())
-                .queryParam("outbound_date", query.getOutbound_date())
-                .queryParam("api_key", apiKey);
-            addQueryParameters(builder, query);
-
-            String url = builder.toUriString();
-            String response = restTemplate.getForObject(url, String.class);
-
-            // JSON 응답 파싱
-            JSONObject jsonObject = new JSONObject(response);
-            System.out.println(jsonObject.toString(0)); // JSON 응답 전체 출력
-
-            JSONArray flightsArray = jsonObject.has("best_flights")
-                ? jsonObject.getJSONArray("best_flights")
-                : jsonObject.getJSONArray("other_flights");
-
-            List<Flight> list = new ArrayList<>();
-            for (int i = 0; i < flightsArray.length(); i++) {
-                JSONObject flightObject = flightsArray.getJSONObject(i);
-                JSONArray flightDetailsArray = flightObject.getJSONArray("flights");
-
-                for (int j = 0; j < flightDetailsArray.length(); j++) {
-                    JSONObject flightDetail = flightDetailsArray.getJSONObject(j);
-                    Flight flight = new Flight();
-
-                    // Set departure and arrival airports
-                    JSONObject departureAirport = flightDetail.getJSONObject("departure_airport");
-                    Airport departure = new Airport();
-                    departure.setId(departureAirport.getString("id"));
-                    departure.setName(departureAirport.getString("name"));
-                    departure.setTime(departureAirport.getString("time"));
-                    flight.setDeparture_airport(departure);
-
-                    JSONObject arrivalAirport = flightDetail.getJSONObject("arrival_airport");
-                    Airport arrival = new Airport();
-                    arrival.setId(arrivalAirport.getString("id"));
-                    arrival.setName(arrivalAirport.getString("name"));
-                    arrival.setTime(arrivalAirport.getString("time"));
-                    flight.setArrival_airport(arrival);
-
-                    // 항공편 세부 정보 추출
-                    flight.setDeparture_id(flightDetail.getJSONObject("departure_airport").getString("id"));
-                    flight.setArrival_id(flightDetail.getJSONObject("arrival_airport").getString("id"));
-                    flight.setStops(flightDetail.optInt("stops", 0));
-                    flight.setMax_price(flightDetail.optInt("price", 0));
-                    flight.setHl(flightDetail.optString("hl", null));
-                    flight.setGl(flightDetail.optString("gl", null));
-                    flight.setCurrency(flightDetail.optString("currency", null));
-                    // ================================================================
-                    flight.setFlight_number(flightDetail.optString("flight_number", "N/A"));
-                    flight.setDuration(flightDetail.optInt("duration", 0));
-                    flight.setAirline(flightDetail.optString("airline", "N/A"));
-                    flight.setAirplane(flightDetail.optString("airplane", "N/A"));
-
-                    list.add(flight);
-                    System.out.println(flight.toString()); // 로그용
-                }
-            }
-            return list;
-
-        } catch (RestClientException e) {
-            throw new Exception("Failed to communicate with the flight API.", e);
-        } catch (Exception e) {
-            throw new Exception("An unexpected error occurred while searching for flights.", e);
-        }
-    }
-
-
-    private void addQueryParameters(UriComponentsBuilder builder, Flight query) throws Exception {
-        if (query.getType() != 1 && query.getType() != 2) {
-            throw new IllegalArgumentException("Invalid type value.");
-        } else if (query.getType() == 2 && query.getReturn_date() == null) {
-            throw new Exception("Return date is required only for round trip flights.");
-        } else if (query.getType() == 1 && query.getReturn_date() != null) {
-            builder.queryParam("return_date", query.getReturn_date());
-        }
-
-        if (query.getTravel_class() >= 1 && query.getTravel_class() <= 4) {
-            builder.queryParam("travel_class", String.valueOf(query.getTravel_class()));
-        }
-        if (query.getStops() >= 0 && query.getStops() <= 3) {
-            builder.queryParam("stops", String.valueOf(query.getStops()));
-        }
-        if (query.getMax_price() != 0) {
-            builder.queryParam("max_price", String.valueOf(query.getMax_price()));
-        }
-        if (query.getHl() != null) {
-            builder.queryParam("hl", String.valueOf(query.getHl()));
-        }
-        if (query.getGl() != null) {
-            builder.queryParam("gl", String.valueOf(query.getGl()));
-        }
-        if (query.getCurrency() != null) {
-            builder.queryParam("currency", String.valueOf(query.getCurrency()));
-        }
-    }
-}
+//package com.otl.air.service;
+//
+//import com.otl.air.entity.Airport;
+//import com.otl.air.dto.Flight;
+//import org.json.JSONArray;
+//import org.json.JSONObject;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.PropertySource;
+//import org.springframework.stereotype.Service;
+//import org.springframework.web.client.RestClientException;
+//import org.springframework.web.client.RestTemplate;
+//import org.springframework.web.util.UriComponentsBuilder;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//@Service
+//@PropertySource("classpath:application-dev.properties")
+//public class FlightRequestService {
+//
+//    @Value("${GOOGLE_FLIGHTS_API_KEY}")
+//    private String apiKey;
+//
+//    @Autowired
+//    private RestTemplate restTemplate;
+//
+//    public List<Flight> searchFlights(Flight query) throws Exception {
+//        try {
+//            String baseUrl = "https://serpapi.com/search.json?engine=google_flights";
+//            UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(baseUrl)
+//                .queryParam("departure_id", query.getDeparture_id())
+//                .queryParam("arrival_id", query.getArrival_id())
+//                .queryParam("type", query.getType())
+//                .queryParam("outbound_date", query.getOutbound_date())
+//                .queryParam("api_key", apiKey);
+//            addQueryParameters(builder, query);
+//
+//            String url = builder.toUriString();
+//            String response = restTemplate.getForObject(url, String.class);
+//
+//            // JSON 응답 파싱
+//            JSONObject jsonObject = new JSONObject(response);
+//            System.out.println(jsonObject.toString(0)); // JSON 응답 전체 출력
+//
+//            JSONArray flightsArray = jsonObject.has("best_flights")
+//                ? jsonObject.getJSONArray("best_flights")
+//                : jsonObject.getJSONArray("other_flights");
+//
+//            List<Flight> list = new ArrayList<>();
+//            for (int i = 0; i < flightsArray.length(); i++) {
+//                JSONObject flightObject = flightsArray.getJSONObject(i);
+//                JSONArray flightDetailsArray = flightObject.getJSONArray("flights");
+//
+//                for (int j = 0; j < flightDetailsArray.length(); j++) {
+//                    JSONObject flightDetail = flightDetailsArray.getJSONObject(j);
+//                    Flight flight = new Flight();
+//
+//                    // Set departure and arrival airports
+//                    JSONObject departureAirport = flightDetail.getJSONObject("departure_airport");
+//                    Airport departure = new Airport();
+//                    departure.setId(departureAirport.getString("id"));
+//                    departure.setName(departureAirport.getString("name"));
+//                    departure.setTime(departureAirport.getString("time"));
+//                    flight.setDeparture_airport(departure);
+//
+//                    JSONObject arrivalAirport = flightDetail.getJSONObject("arrival_airport");
+//                    Airport arrival = new Airport();
+//                    arrival.setId(arrivalAirport.getString("id"));
+//                    arrival.setName(arrivalAirport.getString("name"));
+//                    arrival.setTime(arrivalAirport.getString("time"));
+//                    flight.setArrival_airport(arrival);
+//
+//                    // 항공편 세부 정보 추출
+//                    flight.setDeparture_id(flightDetail.getJSONObject("departure_airport").getString("id"));
+//                    flight.setArrival_id(flightDetail.getJSONObject("arrival_airport").getString("id"));
+//                    flight.setStops(flightDetail.optInt("stops", 0));
+//                    flight.setMax_price(flightDetail.optInt("price", 0));
+//                    flight.setHl(flightDetail.optString("hl", null));
+//                    flight.setGl(flightDetail.optString("gl", null));
+//                    flight.setCurrency(flightDetail.optString("currency", null));
+//                    // ================================================================
+//                    flight.setFlight_number(flightDetail.optString("flight_number", "N/A"));
+//                    flight.setDuration(flightDetail.optInt("duration", 0));
+//                    flight.setAirline(flightDetail.optString("airline", "N/A"));
+//                    flight.setAirplane(flightDetail.optString("airplane", "N/A"));
+//
+//                    list.add(flight);
+//                    System.out.println(flight.toString()); // 로그용
+//                }
+//            }
+//            return list;
+//
+//        } catch (RestClientException e) {
+//            throw new Exception("Failed to communicate with the flight API.", e);
+//        } catch (Exception e) {
+//            throw new Exception("An unexpected error occurred while searching for flights.", e);
+//        }
+//    }
+//
+//
+//    private void addQueryParameters(UriComponentsBuilder builder, Flight query) throws Exception {
+//        if (query.getType() != 1 && query.getType() != 2) {
+//            throw new IllegalArgumentException("Invalid type value.");
+//        } else if (query.getType() == 2 && query.getReturn_date() == null) {
+//            throw new Exception("Return date is required only for round trip flights.");
+//        } else if (query.getType() == 1 && query.getReturn_date() != null) {
+//            builder.queryParam("return_date", query.getReturn_date());
+//        }
+//
+//        if (query.getTravel_class() >= 1 && query.getTravel_class() <= 4) {
+//            builder.queryParam("travel_class", String.valueOf(query.getTravel_class()));
+//        }
+//        if (query.getStops() >= 0 && query.getStops() <= 3) {
+//            builder.queryParam("stops", String.valueOf(query.getStops()));
+//        }
+//        if (query.getMax_price() != 0) {
+//            builder.queryParam("max_price", String.valueOf(query.getMax_price()));
+//        }
+//        if (query.getHl() != null) {
+//            builder.queryParam("hl", String.valueOf(query.getHl()));
+//        }
+//        if (query.getGl() != null) {
+//            builder.queryParam("gl", String.valueOf(query.getGl()));
+//        }
+//        if (query.getCurrency() != null) {
+//            builder.queryParam("currency", String.valueOf(query.getCurrency()));
+//        }
+//    }
+//}
